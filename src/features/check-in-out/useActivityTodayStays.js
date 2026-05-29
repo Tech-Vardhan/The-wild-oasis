5219eb501e5ac25cad3a25eafa076e857f444964
Harsh <hgurjar@mgb.org>
2026-05-29 17:58:22 +0530

    build issue fix

--- src/features/check-in-out/useActivityTodayStays.js
+++ src/features/check-in-out/useActivityTodayStays.js
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getStaysTodayActivity } from "../../services/apiBookings";
 
 export function useActivityTodayStays() {
-  const { isLoading: isLoading, data: activities } = useQuery({
+  const { isLoading, data: activities } = useQuery({
     queryFn: getStaysTodayActivity,
     queryKey: ["today-activity"],
   });
